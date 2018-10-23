cca3b6164b48e7666a5e3dd6fd7caa8e8c11ce0a
BernardoLaing <A01206492@itesm.mx>
2018-10-22 23:33:04 -0500

    lecture16

--- Notes/notes.js
+++ Notes/notes.js
@@ -1,7 +1,25 @@
 console.log('Starting notes.js');
 
+const fs =  require('fs');
+
 var addNote = (title, body) => {
-  console.log('Adding note', title, body);
+  var notes = [];
+  var note = {
+    title: title,
+    body: body
+  };
+
+  try{
+    notes = JSON.parse(fs.readFileSync('notes-data.json'));
+  }catch (e){
+
+  }
+
+  var duplicates = notes.filter((note) => note.title === title);
+  if(duplicates.length === 0){
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
