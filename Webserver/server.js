dbd50a5cde6156e0f65c515dff3f91314c2b2ec9
BernardoLaing <A01206492@itesm.mx>
2018-10-28 08:02:36 -0600

    setup startscript for heroku

--- Webserver/server.js
+++ Webserver/server.js
@@ -2,6 +2,8 @@ const express   = require('express');
 const hbs       = require('hbs');
 const fs        = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -16,7 +18,7 @@ app.use((req, res, next) => {
   });
   next();
 });
-// 
+//
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs', {
 //     pageTitle: 'Maintenance Page'
@@ -53,4 +55,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000)
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`);
+});
